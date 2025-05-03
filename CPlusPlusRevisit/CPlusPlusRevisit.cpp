d4db710c2f002fed775e813b76b19f854464ecab
alecallain <alecjallain@gmail.com>
2025-05-03 17:37:20 -0400

    Initial window rendering

--- CPlusPlusRevisit/CPlusPlusRevisit.cpp
+++ CPlusPlusRevisit/CPlusPlusRevisit.cpp
@@ -6,9 +6,58 @@
 
 using namespace std;
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Initialization
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+    // Configuring GLFW window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "3D Modeling", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Accounts for window resizing
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // Load GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // Main render loop
+    while (!glfwWindowShouldClose(window)) {
+        // input
+        processInput(window);
+        
+        // rendering
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // refresh
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
     return 0;
 }
 
